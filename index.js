6ee9d704e9d23d9694f47bcf85c6d3d801cd31ac
SamR0429 <Samanatha.ramos01@gmail.com>
2023-08-17 20:17:10 -0500

    checkpoint ending for the day

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const artContainer = document.getElementById('art-carousel');
+
+
+// async function gettingArtInfo(artName){
+//     let results = await fetch (`https://api.artic.edu/api/v1/artworks`);
+//     let data = await results.json();
+//     console.log(data);
+//
+//     // renderingCarouselCards(data)
+// }
+
+
+
+
+
+
+
+
+
+function renderingCarouselCards(artInfo){
+
+   // let artName = artInfo.title;
+   // let art
+
+
+
+    artContainer.innerHTML = `
+
+
+
+
+    `;
+}
+
+gettingArtInfo();
